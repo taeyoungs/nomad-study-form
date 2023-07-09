f410d9db27fbdcf2fab505d1d59f6481ff8744ff
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-07-09 12:24:37 +0900

    mod: Google Font(Anek Bangla) 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
+import { Anek_Bangla } from 'next/font/google';
+
+const anekBangla = Anek_Bangla({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={anekBangla.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
